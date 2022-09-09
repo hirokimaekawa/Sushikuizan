064a5c89d9557f3d0569caab34c5bf8aafb3d94e
hirokimaekawa <43659796+hirokimaekawa@users.noreply.github.com>
2022-09-09 21:42:51 +0900

    更新

--- Assets/Script/Mushikuizan.cs
+++ Assets/Script/Mushikuizan.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Mushikuizan : MonoBehaviour
 {
@@ -11,11 +12,21 @@ public class Mushikuizan : MonoBehaviour
     int rightNumber;
     int sumNumber;
 
+    int count; // デフォルトで20とかにしておいて、設定で変えれるようにする
+
     public TextMeshProUGUI sumNumberText;
     public TextMeshProUGUI leftNumberText;
     public TextMeshProUGUI rightNumberText;
     public GameObject squareText;　　//Textだけど、GameObject型で取得した
+    public GameObject answerImage;
+    public GameObject finishPanel;
 
+    AudioSource audioSource;
+    public AudioClip correctSE;
+    public AudioClip incorrectSE;
+    public AudioClip successSE;
+
+    //以下、11個の宣言はもう必要ない
     public TextMeshProUGUI textMeshProUGUI_0;
     public TextMeshProUGUI textMeshProUGUI_1;
     public TextMeshProUGUI textMeshProUGUI_2;
@@ -31,44 +42,82 @@ public class Mushikuizan : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         CreateQuestion();
-        ExtractTextInt();
+         //ExtractTextInt();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        Finish();
+    }
+
+    bool isCalledOnce = false;
+    void Finish()
+    {
+        if (count == 10 && isCalledOnce ==false)
+        {
+            finishPanel.SetActive(true);
+            audioSource.PlayOneShot(successSE);  //Updateに入っているから、ずっと鳴りっぱなし。
+            isCalledOnce = true;
+        }
     }
 
     void CreateQuestion()
     {
+        Init();
         //SystemとUnityEngineの両方があると、Random.Rangeは衝突して使えなくなる
         sumNumber = UnityEngine.Random.Range(1,11); // 答えは、１〜１０までの数字をランダムで抽出する
         //Debug.Log(sumNumber);
         sumNumberText.text = sumNumber.ToString();
-        leftNumber = UnityEngine.Random.Range(0, sumNumber+1); //左の数字は、０〜sumNumner+1まで
+        leftNumber = UnityEngine.Random.Range(0, sumNumber); //左の数字は、０〜sumNumner+1まで⇨右辺だけでなく、左辺も10になったら、テキストが黒板からはみ出すからsumNumberにした
         //Debug.Log(leftNumber);
         leftNumberText.text = leftNumber.ToString();
         rightNumber = sumNumber - leftNumber; //右の数字は、sumNumberからleftNumberを引いて求める
         Debug.Log(rightNumber);
     }
+    
 
-    void CheckAnswer()
+    IEnumerator appearAnwser()
     {
-        //０〜１０までのボタンにそれぞれ、０〜１０のint型の整数として識別するには、どうしたら良いのか？
-
-        //if (rightNumber = 押したボタンの数字と一緒なら)
-        //{
-
-        //}
+        if (count < 9)
+        {
+            ShowCorrectSE();
+            squareText.SetActive(false);
+            rightNumberText.text = rightNumber.ToString();
+            answerImage.SetActive(true);
+            count++;
+            Debug.Log("カウントは"+count);
+            yield return new WaitForSeconds(1.0f);
+            CreateQuestion();
+        }
+        else
+        {
+            ShowCorrectSE();
+            squareText.SetActive(false);
+            rightNumberText.text = rightNumber.ToString();
+            answerImage.SetActive(true);
+            count++;
+            Debug.Log(" 最後のカウントは" + count);
+        }
+        
     }
 
-    void appearAnwser()
+    void Init()
     {
-        squareText.SetActive(false); 
-        rightNumberText.text = rightNumber.ToString();
+        squareText.SetActive(true);
+        rightNumberText.text = "";
+        answerImage.SetActive(false);
     }
+
+    void Reset()
+    {
+        count = 0;
+        isCalledOnce = false; //一回だけ呼び出すために bool配置
+        CreateQuestion();
+    }
+
     void ExtractTextInt()
     {
         //Debug.Log(textMeshProUGUI_0.text);
@@ -78,4 +127,156 @@ public class Mushikuizan : MonoBehaviour
         Debug.Log(y);
     }
 
+    void ShowCorrectSE()
+    {
+        audioSource.PlayOneShot(correctSE);
+    }
+
+    void ShowInCorrectSE()
+    {
+        audioSource.PlayOneShot(incorrectSE);
+    }
+
+    public void SelectNumber_0()
+    {
+        int number = 0;
+        if (number == rightNumber)
+        {
+            StartCoroutine(appearAnwser());
+        }
+        else
+        {
+            ShowInCorrectSE();
+        }
+    }
+    public void SelectNumber_1()
+    {
+        int number = 1;
+        if (number == rightNumber)
+        {
+            StartCoroutine(appearAnwser());
+        }
+        else
+        {
+            ShowInCorrectSE();
+        }
+    }
+    public void SelectNumber_2()
+    {
+        int number = 2;
+        if (number == rightNumber)
+        {
+            StartCoroutine(appearAnwser());
+        }
+        else
+        {
+            ShowInCorrectSE();
+        }
+    }
+    public void SelectNumber_3()
+    {
+        int number = 3;
+        if (number == rightNumber)
+        {
+            StartCoroutine(appearAnwser());
+        }
+        else
+        {
+            ShowInCorrectSE();
+        }
+    }
+    public void SelectNumber_4()
+    {
+        int number = 4;
+        if (number == rightNumber)
+        {
+            StartCoroutine(appearAnwser());
+        }
+        else
+        {
+            ShowInCorrectSE();
+        }
+    }
+    public void SelectNumber_5()
+    {
+        int number = 5;
+        if (number == rightNumber)
+        {
+            StartCoroutine(appearAnwser());
+        }
+        else
+        {
+            ShowInCorrectSE();
+        }
+    }
+    public void SelectNumber_6()
+    {
+        int number = 6;
+        if (number == rightNumber)
+        {
+            StartCoroutine(appearAnwser());
+        }
+        else
+        {
+            ShowInCorrectSE();
+        }
+    }
+    public void SelectNumber_7()
+    {
+        int number = 7;
+        if (number == rightNumber)
+        {
+            StartCoroutine(appearAnwser());
+        }
+        else
+        {
+            ShowInCorrectSE();
+        }
+    }
+    public void SelectNumber_8()
+    {
+        int number = 8;
+        if (number == rightNumber)
+        {
+            StartCoroutine(appearAnwser());
+        }
+        else
+        {
+            ShowInCorrectSE();
+        }
+    }
+    public void SelectNumber_9()
+    {
+        int number = 9;
+        if (number == rightNumber)
+        {
+            StartCoroutine(appearAnwser());
+        }
+        else
+        {
+            ShowInCorrectSE();
+        }
+    }
+    public void SelectNumber_10()
+    {
+        int number = 10;
+        if (number == rightNumber)
+        {
+            StartCoroutine(appearAnwser());
+        }
+        else
+        {
+            ShowInCorrectSE();
+        }
+    }
+    public void RetryButton()
+    {
+        Reset();
+        finishPanel.SetActive(false);
+    }
+
+    public void ToTitleButton()
+    {
+        SceneManager.LoadScene("Title");
+    }
 }
