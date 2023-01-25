2360ca14662aed7e5b2f9281989f7699297478f8
hirokimaekawa <43659796+hirokimaekawa@users.noreply.github.com>
2023-01-25 19:43:38 +0900

    更新

--- Assets/Script/DragSushi.cs
+++ Assets/Script/DragSushi.cs
@@ -21,7 +21,11 @@ public class DragSushi : MonoBehaviour,IDragHandler ,IBeginDragHandler,IEndDragH
     }
     public void OnDrag(PointerEventData eventData)
     {
+        Debug.Log(eventData.position);
         transform.position = eventData.position;
+
+        //120f * Screen.width / 750f,650f * Screen.width / 750f
+        transform.position = new Vector3(Mathf.Clamp(eventData.position.x,0.03f*Screen.width,0.98f*Screen.width), Mathf.Clamp(eventData.position.y, 130f * Screen.height / 750f, 620f * Screen.height / 750f),transform.position.z);
     }
 
     public void OnBeginDrag(PointerEventData eventData)
